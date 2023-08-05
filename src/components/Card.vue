9c4342d6f742cc75fcaf00738b2ce29364dc51da
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-08-05 14:12:50 +0900

    Add animation

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,26 +1,90 @@
 <template>
-  <div class="body__container">
-    <div class="body__profile">
-      <Photo />
-      <Buttons />
-    </div>
-  </div>
+  <vue-flip height="100%" width="100%" v-model="show">
+    <template v-slot:front>
+      <div class="body__container">
+        <div class="body__profile">
+          <Photo :korean="korean" />
+          <div class="body__profile__button-area" id="ko-kr">
+            <Korean />
+            <Social />
+            <button class="body__profile__button github">
+              <a href="https://www.github.com/RAprogramm">GitHub에서 포트폴리오 보기</a>
+            </button>
+            <button class="body__profile__button cv">
+              <a v-bind:href="resume">이력서를 PDF로 다운로드</a>
+            </button>
+          </div>
+
+          <div class="body__profile__translate">
+            <button @click="translate" v-on:click="fadeMe">
+              <fa icon="flag" /> Translate {{ enFlag }}
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </template>
+
+    <template v-slot:back>
+      <div class="body__container">
+        <div class="body__profile">
+          <Photo :korean="korean" />
+          <div  class="body__profile__button-area">
+            <English />
+            <Social />
+            <button class="body__profile__button github botao">
+              <a href="https://www.github.com/RAprogramm">View portfolio on GitHub</a>
+            </button>
+            <button class="body__profile__button cv botao">
+              <a v-bind:href="resume">Download Resume in PDF </a>
+            </button>
+          </div>
+          <div class="body__profile__translate">
+            <button @click="translate" v-on:click="fadeMe">
+              <fa icon="flag" /> 번역하기 {{ krFlag }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </template>
+  </vue-flip>
 </template>
 
 <script>
 import Photo from './Photo'
-import Buttons from './Buttons'
+import { VueFlip } from 'vue-flip'
+import Korean from './Korean'
+import Social from './Social'
+import English from './English'
 
 export default {
   name: 'Card',
+  data() {
+    return {
+      trans: Boolean(true),
+      korean: Boolean,
+      resume:
+        'https://github.com/RAprogramm/RAprogramm/files/12246611/AndreiRozanov.pdf',
+      krFlag: '(KR)',
+      enFlag: '(EN)',
+      show: false
+    }
+  },
   components: {
     Photo,
-    Buttons
+    'vue-flip': VueFlip,
+    Korean,
+    English,
+    Social
   },
   methods: {
-    translateBio() {
-      console.log('translate bio')
-    }
+     translate() {
+       this.korean = !this.korean
+       this.trans = !this.trans
+     },
+     fadeMe: function () {
+       this.show = !this.show
+     },
   }
 }
 </script>
@@ -40,5 +104,62 @@ export default {
   margin: 2em auto;
   max-width: 330px;
   box-shadow: 0px 3px 136px 0px rgba(88, 87, 119, 0.21);
+}
+
+.body__profile__button-area {
+  top: -40px;
+  position: relative;
+}
+
+.body__profile button a {
+  text-decoration: none;
+  color: white;
+}
+
+.body__profile button {
+  border: none;
+  border-radius: 10px;
+  font-family: Orkney;
+  text-transform: uppercase;
+  padding: 1.5em 1em;
+  width: 100%;
+  color: white;
+  letter-spacing: 2px;
+  font-weight: bold;
+  margin: 0.5em 0;
+  cursor: pointer;
+  transition: all 0.5s;
+}
+
+.github {
+  background-color: #7ed8a7;
+}
+
+.github:hover {
+  background-color: #66df6d;
+  box-shadow: 3px 3px 5px gray;
+}
+
+.cv {
+  background-color: #54dd89;
+}
+
+.cv:hover {
+  background-color: #54e53d;
+  box-shadow: 3px 3px 5px gray;
+}
+
+.body__profile__translate button {
+  font-size: 0.75em;
+  border: 0;
+  background-color: transparent;
+  padding: 0;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  color: #999;
+  top: -10px;
+  cursor: pointer;
+  position: relative;
+  transition: all 0.5s;
 }
 </style>
--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,22 +1,85 @@
 <template>
-  <div class="body__profile__photo">
-    <img src="../assets/face.jpg" alt="Face" />
-  </div>
+  <transition
+    mode="out-in"
+    name="bounce"
+    appear
+  >
+    <div
+      v-if="korean"
+      class="body__profile__photo"
+    >
+      <img
+        src="../assets/face.jpg"
+        alt="Face"
+      />
+    </div>
+    <div
+      v-else
+      class="body__profile__photo2"
+    >
+      <img
+        src="../assets/face2.jpg"
+        alt="in Korea"
+      />
+    </div>
+  </transition>
 </template>
 
 <script>
-export default {
-  name: 'Photo'
-}
+  export default {
+    name: 'Photo',
+    props: ['korean']
+  }
 </script>
 
 <style>
-.body__profile__photo img {
-  border-radius: 50%;
-  height: 250px;
-  border: 15px solid #fff;
-  top: -100px;
-  position: relative;
-  box-shadow: 1px 2px 18px 4px rgb(226, 226, 226);
-}
+  .bounce-enter-active {
+    animation: bounce-in 0.5s;
+  }
+
+  .bounce-leave-active {
+    animation: bounce-in 0.5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+
+    50% {
+      transform: scale(1.25);
+    }
+
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 1s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+
+  .body__profile__photo img {
+    border-radius: 50%;
+    height: 250px;
+    border: 15px solid #fff;
+    top: -100px;
+    position: relative;
+    box-shadow: 1px 2px 18px 4px rgb(226, 226, 226);
+  }
+
+  .body__profile__photo2 img {
+    border-radius: 50%;
+    height: 250px;
+    border: 15px solid #fff;
+    top: -100px;
+    position: relative;
+    box-shadow: 1px 2px 18px 4px rgb(226, 226, 226);
+  }
 </style>
