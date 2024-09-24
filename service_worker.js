d5f41dcb49cafe8b40f87af7f722fba8b5734606
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-09-24 17:23:34 +0900

    add: gh actions

--- service_worker.js
+++ service_worker.js
@@ -1,36 +1,54 @@
-var cacheName = 'rozanov-dev';
-var filesToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './fonts/',
-  './images/',
-  './business-card-19a0b2818c80bf44.js',
-  './business-card-19a0b2818c80bf44_bg.wasm',
-  './card-967bf27f107bc36d.css',
-  './common-72e6c7e34c655ccd.css',
-  './english-1ca621f4ecf4e5e.css',
-  './korean-3a4be8182e2d81a6.css',
-  './russian-ca2210277db8d5ca.css',
-  './photo-4c57af31f4f3e898.css',
-  './social-6b8195cf07663bd0.css',
-  './service_worker.js'
-];
+var cacheName = 'rozanov-dev-v1';
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
+// Динамическое кэширование при первой загрузке ресурсов
+self.addEventListener('install', function(event) {
+  event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
     })
   );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+/* Динамическое кэширование запрашиваемых ресурсов */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Если ресурс уже есть в кэше, возвращаем его
+      if (response) {
+        return response;
+      }
+
+      // Если ресурса нет в кэше, загружаем его и кэшируем
+      return fetch(event.request).then(function(networkResponse) {
+        return caches.open(cacheName).then(function(cache) {
+          // Исключаем из кэширования файлы с запросами через метод POST
+          if (event.request.method === 'GET') {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
+
+/* Очистка старого кэша при обновлении Service Worker */
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cache) {
+          if (cacheWhitelist.indexOf(cache) === -1) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
