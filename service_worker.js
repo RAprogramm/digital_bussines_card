bdcbe38ea0eeaf526eff6c8849efbbab5d3f6a06
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-09-24 15:48:30 +0900

    update: service worker

--- service_worker.js
+++ service_worker.js
@@ -2,10 +2,19 @@ var cacheName = 'rozanov-dev';
 var filesToCache = [
   './',
   './index.html',
-  // './pkg/bundle.js',
-  // './pkg/yew_wasm_pack_minimal_bg.wasm'
+  './manifest.json',
+  './business-card-a2f87776afbff675df192f5ef0b8c15f04dd34cdadbb95453ffc499a0b30ccc256aca3e537d9ae023f93c508b37da209.js',
+  './business-card-a2f87776afbff675df192f5ef0b8c15f04dd34cdadbb95453ffc499a0b30ccc256aca3e537d9ae023f93c508b37da209_bg.wasm',
+  './card-b51b57eb686c50e4.css',
+  './common-d9100199d73e14d.css',
+  './english-da5bbfaaf645a9cc.css',
+  './korean-8096dc6a98b61425.css',
+  './russian-75ade782d1a8cd34.css',
+  './photo-279ef7f03220109b.css',
+  './social-86957b08a03d9950.css',
+  './fonts/',
+  './images/'
 ];
-
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
